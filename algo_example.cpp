602ee2d095cb05d3240d323186ae8fc2455780be
Richard Hickling <richard@profitview.net>
2021-01-25 17:54:36 +0100

    Correcting argument position

--- algo_example.cpp
+++ algo_example.cpp
@@ -1,33 +1,12 @@
 #include "cpp_crypto_algosConfig.h"
-#include <Bitmex.h>
-#include "Poco/ConsoleChannel.h"
-#include "Poco/Thread.h"
-#include "SIOClient.h"
-#include "DataStreamer.h"
 #include "TalibMeanReversion.h"
+#include "profitview_util.h"
+#include <Bitmex.h>
+#include "SIOClient.h"
 #include "Poco/URI.h"
-#include "Poco/JSON/Array.h"
-#include "Poco/JSON/Object.h"
-#include "Poco/JSON/Parser.h"
-#include "Poco/Any.h"
+#include "Poco/ConsoleChannel.h"
 
 #include <iostream>
-
-using Poco::Thread;
-using Poco::URI;
-using Poco::Dynamic::Var;
-using Poco::JSON::Parser;
-
-auto constructSymbolJSON(int number_of_symbols, char** symbols)
-{
-	std::string symbols_json_string{"[\""};
-	symbols_json_string += symbols[0] + std::string{"\""};
-	for (int n = 0; n < number_of_symbols; ++n)
-		symbols_json_string += ",\"" + std::string{symbols[n]} + "\"";
-	symbols_json_string += "]";
-
-	return Parser().parse(symbols_json_string).extract<Poco::JSON::Array::Ptr>();
-}
 
 int main(int argc, char *argv[])
 {
@@ -36,7 +15,7 @@ int main(int argc, char *argv[])
 		api_key_arg{1}, api_secret_arg{2}, 
 		profitview_api_arg{3}, 
 		lookback_arg{4}, reversion_level_arg{5}, base_quantity_arg{6}, 
-		symbol_args{8};
+		symbol_args{7};
 
 	if (argc < symbol_args) {
 		// report version
@@ -50,16 +29,17 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	Logger *logger = &(Logger::get("example"));
+	Logger *logger {&(Logger::get("example"))};
 
 	logger->setChannel(new Poco::ConsoleChannel());
 
 	Bitmex bitmex{argv[api_key_arg], argv[api_secret_arg]};
-	TalibMeanReversion *algo = new TalibMeanReversion(
-		bitmex, std::stoi(argv[lookback_arg]), std::stod(argv[reversion_level_arg]), std::stoi(argv[base_quantity_arg]));
+	TalibMeanReversion *algo { new TalibMeanReversion(
+		bitmex, std::stoi(argv[lookback_arg]), std::stod(argv[reversion_level_arg]), std::stoi(argv[base_quantity_arg]))};
 
 	logger->information("Creating URI\n");
 
+	using Poco::URI;
 	//Establish the socket.io connection
 	URI connect_uri{"https://markets.profitview.net"};
 
@@ -81,7 +61,7 @@ int main(int argc, char *argv[])
 
 		logger->information("Socket.io client setup complete\n");
 
-		sioUserClient->emit("subscribe", constructSymbolJSON(argc - symbol_args, argv + symbol_args));
+		sioUserClient->emit("subscribe", profitview::util::constructSymbolJSON(argc - symbol_args, argv + symbol_args));
 
 		logger->information("Press ENTER to quit...");
 		std::cin.get();
