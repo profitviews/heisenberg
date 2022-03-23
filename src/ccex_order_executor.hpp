ecfc966e6d9632434f2ea05322f8f3010d3896f1
Richard Hickling <richard.t.hickling@gmail.com>
2022-03-23 13:00:20 +0100

    Adding todo for market orders

--- src/ccex_order_executor.hpp
+++ src/ccex_order_executor.hpp
@@ -98,6 +98,9 @@ public:
         Session session(session_options, session_configs, &event_handler);
 
         Request request(Request::Operation::CREATE_ORDER, exchange_, symbol);
+
+        // @todo Properly handle Market orders.  Some exchanges don't have native Market orders
+        //       Therefore create a type of cross limit to substitute
         request.appendParam({{"type", type == OrderType::Market ? "market" : "limit"},
                             {"side", side == Side::Buy ? "BUY" : "SELL"},
                             {"size", std::to_string(orderQty)},
