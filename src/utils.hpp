ba95cf51f0ae4505d4174bf0ed3791c0a09e99e4
Richard Hickling <richard.t.hickling@gmail.com>
2022-04-10 11:05:52 +0200

    Reordering functions for presentation

--- src/utils.hpp
+++ src/utils.hpp
@@ -34,10 +34,31 @@ void print_trade_data(TradeData const& trade_data)
     fmt::print("Time: {}", std::asctime(std::localtime(&trade_data.time)));
 }
 
+class CsvWriter : public csv2::Writer<csv2::delimiter<','>>
+{    // Provides shorthand
+     // `write_strings()`
+public:
+    CsvWriter(auto& stream)
+        : csv2::Writer<csv2::delimiter<','>>(stream)
+    {}
+
+    void write_strings(auto&&... args)
+    {
+        write_row<std::vector<std::string>>({fmt::format("{}", std::forward<decltype(args)>(args))...});
+    }
+};
+
 // boost::accumulate should do this, but there's a compile fail
 inline auto accumulate(const auto& s, auto i) -> auto { return std::accumulate(std::begin(s), std::end(s), i); }
 
 inline auto accumulate(auto& s, auto i, auto op) -> auto { return std::accumulate(std::begin(s), std::end(s), i, op); }
+
+auto stdev(auto& s, auto m, int p) -> auto
+{    // Calculate standard deviation given mean (m)
+    auto const variance{[&m, &p](auto a, const auto& v) { return a + (v - m) * (v - m) / (p - 1);}};
+
+    return std::sqrt(accumulate(s, 0.0, variance));
+}
 
 auto ma(auto const& s, int p = 0) -> auto { return accumulate(s, 0.0) / (p ? p : s.size()); }
 
@@ -49,13 +70,6 @@ auto ema(auto const& s, auto p, auto m = 0) -> auto
     auto const ema_step{[&alpha](auto a, const auto& price) { return price * alpha + a * (1 - alpha); }};
 
     return accumulate(s, m, ema_step);
-}
-
-auto stdev(auto& s, auto m, int p) -> auto
-{    // Calculate standard deviation given mean (m)
-    auto const variance{[&m, &p](auto a, const auto& v) { return a + (v - m) * (v - m) / (p - 1);}};
-
-    return std::sqrt(accumulate(s, 0.0, variance));
 }
 
 auto abs_differences(const auto& prices, int e) -> auto
@@ -95,17 +109,4 @@ auto is_monotonic(auto const& s) -> std::tuple<bool, bool>    // { monotonic, up
     return {true, prev == 1};
 }
 
-class CsvWriter : public csv2::Writer<csv2::delimiter<','>>
-{    // Provides shorthand
-     // `write_strings()`
-public:
-    CsvWriter(auto& stream)
-        : csv2::Writer<csv2::delimiter<','>>(stream)
-    {}
-
-    void write_strings(auto&&... args)
-    {
-        write_row<std::vector<std::string>>({fmt::format("{}", std::forward<decltype(args)>(args))...});
-    }
-};
 }    // namespace profitview::util
